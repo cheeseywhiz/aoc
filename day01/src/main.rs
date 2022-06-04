bbc152dea0ac37f7582c27cc62e3cb614465d1de
Spencer Todd <sctodd@umich.edu>
2022-06-04 16:14:42 -0400

    day01: part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,20 +3,31 @@ use std::io::{self, Read};
 fn main() {
     let mut lines = String::new();
     io::stdin().read_to_string(&mut lines).unwrap();
-    let mut depth: Option<usize> = None;
+    let depths = lines
+        .split('\n')
+        .filter(|line| !line.is_empty())
+        .map(|line| str::parse(line).unwrap())
+        .collect::<Vec<usize>>();
+    eprintln!("{depths:#?}");
+    let windows = depths.windows(3).collect::<Vec<_>>();
+    eprintln!("{windows:#?}");
+    let mut sum: Option<usize> = None;
     let mut answer = 0;
 
-    for line in lines.split('\n').filter(|line| !line.is_empty()) {
-        let depth2: usize = str::parse(line).unwrap();
+    for window in windows {
+        eprintln!("{window:#?}");
+        let sum2 = window.iter().sum();
+        eprintln!("{sum2}");
 
-        if let Some(depth) = depth {
-            if depth2 > depth {
+        if let Some(sum) = sum {
+            if sum2 > sum {
                 answer += 1;
             }
+
         }
 
-        depth = Some(depth2)
+        sum = Some(sum2);
     }
 
-    println!("answer: {answer}");
+    eprintln!("answer: {answer}");
 }
